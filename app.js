f2fd90c92b5f0ac3027cd5628fce7bbe95eaaea1
Helevetios <juaniak109@outlook.es>
2025-02-25 19:52:42 -0600

    spanish translate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let table;
 const previousPrices = {};
+var language = {}
 
 async function getTop20Coins() {
   try {
@@ -47,17 +48,34 @@ async function getTop20Coins() {
   }
 }
 
-$(document).ready(function () {
-  table = $('#cryptoTable').DataTable({
-    paging: true,
-    searching: true,
-    info: false,
-    order: []
+
+async function cargarJSON() {
+  try {
+    const response = await fetch('/es-ES.json'); // Ruta relativa o absoluta
+    const data = await response.json(); // Convertir a objeto JavaScript
+    return data; // Asignar a una variable si lo deseas
+  } catch (error) {
+    console.error('Error al cargar el JSON:', error);
+  }
+}
+
+cargarJSON().then(data => {
+  language = data;
+
+  $(document).ready(function () {
+    table = $('#cryptoTable').DataTable({
+      paging: true,
+      searching: true,
+      info: false,
+      order: [],
+      language: language
+    });
   });
 
   getTop20Coins();
   setInterval(getTop20Coins, 10000);
 });
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const refresh = document.getElementById('refresh-button');
